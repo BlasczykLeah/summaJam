ef9b963f12b7fbc7b72d721c5062912d897bfaf6
WesMcW <wesmcwhorter@gmail.com>
2020-06-26 20:26:44 -0500

    Fine

---
+++ jamjam/Assets/Scripts/CloudMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour
+{
+    public float speed;
+    Vector3 target, start;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = transform.position;
+        //target = new Vector3(37, 3.47f, 28.13f);
+        target = new Vector3(81, -1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.fixedDeltaTime);
+
+        if(transform.position == target)
+        {
+            transform.position = start;
+        }
+    }
+}
