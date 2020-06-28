2969043307a77e7349c80b47826e9f4d99979946
WesMcW <wesmcwhorter@gmail.com>
2020-06-27 19:33:16 -0500

    Better tits

---
+++ jamjam/Assets/Scripts/ButtonStuff.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonStuff : MonoBehaviour
+{
+    public GameObject buttons, inside, gameDesc, knight, thief, chungus, arcanist, credits;
+    public Animator doorAnim;
+
+    public void OpenTheGate()
+    {
+        buttons.SetActive(false);
+        gameDesc.SetActive(true);
+        knight.SetActive(false);
+        thief.SetActive(false);
+        chungus.SetActive(false);
+        arcanist.SetActive(false);
+        credits.SetActive(false);
+        doorAnim.SetTrigger("Open");
+    }
+
+    public void CloseTheGate()
+    {
+        inside.SetActive(false);
+        doorAnim.SetTrigger("Close");
+    }
+
+    public void OpenKnight()
+    {
+        gameDesc.SetActive(false);
+        knight.SetActive(true);
+    }
+
+    public void OpenThief()
+    {
+        knight.SetActive(false);
+        thief.SetActive(true);
+    }
+
+    public void OpenChungus()
+    {
+        thief.SetActive(false);
+        chungus.SetActive(true);
+    }
+
+    public void OpenArcanist()
+    {
+        chungus.SetActive(false);
+        arcanist.SetActive(true);
+    }
+
+    public void OpenCredits()
+    {
+        arcanist.SetActive(false);
+        credits.SetActive(true);
+    }
+}
